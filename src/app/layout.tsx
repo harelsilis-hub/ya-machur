600dd840d077fef93dab18058a991c3ea18f0740
Harel Silis <harel.silis@gmail.com>
2026-03-30 20:58:27 +0300

    new logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,11 @@ export const metadata: Metadata = {
   title: 'Ya Machur',
   description: 'Minimalist Focus Enforcer',
   manifest: '/manifest.json',
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/icon.png',
+    apple: '/apple-icon.png',
+  },
   appleWebApp: {
     title: 'Ya Machur',
     statusBarStyle: 'black-translucent',
